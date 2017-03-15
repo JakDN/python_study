94c52ca962545015e489cc460ef48bd54f90581d
jak <jak@jak.com>
2017-03-16 00:03:27 +0800

    change test6

--- nan6.py
+++ nan6.py
@@ -2,10 +2,13 @@
 
 a = int(raw_input("a:"))
 b = int(raw_input("b:"))
-if a>b:
-    print "a is big"
-if a==b:
-    print "a is as big as b"
-if a<b:
-    print "b is bigger"
+def compare(a,b):
+    if a>b:
+        print "a is big"
+    if a==b:
+        print "a is as big as b"
+    if a<b:
+        print "b is bigger"
+
+compare(a,b)
 
