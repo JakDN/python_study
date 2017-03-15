68e400aa526ef60fb11f4d63cf950808a172b3d9
jak <jak@jak.com>
2017-03-15 22:40:44 +0800

    def fun

---
+++ nan5.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+
+a = int(raw_input("please input a:"))
+b = int(raw_input("please input b:"))
+def add(a,b):
+    c = a+b
+    d = a-b
+    e = a*b
+    f = a/b
+    print "this is a+b:",c
+    print "this is a-b:",d
+    print "this is a*b:",e
+    print "this is a/b:",f
+ 
+add(a,b)
