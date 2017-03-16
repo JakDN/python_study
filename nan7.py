07fd0d8c342ad52c52a4551527616af74bd6b42d
nan <nan@nan.com>
2017-03-16 23:46:24 +0800

    new nan7&nan8

---
+++ nan7.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#coding:utf8
+
+def machine(a,b="巧克力"):
+    print "这是一个",a,"元",b,"口味的雪糕！"
+
+x = raw_input("input price:")
+y = raw_input("input tast:")
+
+machine(x,y)
+
+
+#参数的默认值必须从右往左定义
+#
