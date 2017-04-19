94e57432f6bdef16615f06a723676a4c58bdbfe0
jak <jak@jak.com>
2017-04-19 22:29:11 +0800

    modified guess_age

--- guess_age.py
+++ guess_age.py
@@ -3,11 +3,26 @@
 
 age_of_man = 43
 
-guess_age = int(raw_input("guess age:"))
-
+# guess_age = int(raw_input("guess age:"))
+# 循坏执行
+'''
 if guess_age == age_of_man:
     print ("yes you get it!")
 elif guess_age > age_of_man:
     print ("think smaller...")
 else:
     print ("think bigger!")
+'''
+count = 0
+while count < 5:
+    guess_age = int(raw_input("guess age:"))
+    if guess_age == age_of_man:
+        print ("yes you get it")
+        break
+    elif guess_age < age_of_man:
+        print ("think bigger...")
+    else:
+        print ("think smaller!")
+    count += 1
+else:
+    print ("you try too more times")
